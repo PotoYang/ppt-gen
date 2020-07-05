c2cdbe70b77a56749ddc91f84fba5563140a360d
PotoYang <715792648@qq.com>
2020-07-05 23:42:54 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,136 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+            <el-button type="primary" @click="init()">init</el-button>
+            <el-button type="primary" @click="isShow=true">aaa</el-button>
+        </el-dialog>
+
+        <div class="reveal" v-if="isShow" style="height: 800px; margin: 0">
+            <div class="slides">
+                <section>
+                    <h2>图</h2>
+                    <div id="pieReport" style="height: 600px; width: 600px;"></div>
+                </section>
+                <section>
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>卡片名称</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                        </div>
+                        <div v-for="o in 4" :key="o" class="text item">
+                            {{'列表内容 ' + o }}
+                        </div>
+                    </el-card>
+                    <h2>Stretch Example</h2>
+                    <p>Image byline</p>
+                </section>
+                <section>Horizontal Slide</section>
+                <section>
+                    <section>Vertical Slide 1</section>
+                    <section>Vertical Slide 1</section>
+                    <section>Vertical Slide 1</section>
+                </section>
+                <section>Horizontal Slide</section>
+                <section>
+                    <section>Vertical Slide 1</section>
+                    <section>Vertical Slide 1</section>
+                    <section>Vertical Slide 1</section>
+                </section>
+                <section>Horizontal Slide</section>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import 'reveal.js/dist/reveal.css'
+    import 'reveal.js/dist/theme/white.css'
+
+    import Reveal from 'reveal.js'
+
+    import Echarts from 'echarts'
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                dialogFormVisible: true,
+                isShow: false,
+                charts: "",
+                opinion: ["及格人数", "未及格人数"],
+                opinionData: [
+                    {value: 12, name: "及格人数", itemStyle: "#1ab394"},
+                    {value: 18, name: "未及格人数", itemStyle: "#79d2c0"}
+                ]
+            }
+        },
+        methods: {
+            init() {
+                new Reveal().initialize();
+                this.drawPie("pieReport");
+            },
+            drawPie(id) {
+                this.charts = Echarts.init(document.getElementById(id));
+                this.charts.setOption({
+                    tooltip: {
+                        trigger: "item",
+                        formatter: "{a}<br/>{b}:{c} ({d}%)"
+                    },
+                    legend: {
+                        bottom: 10,
+                        left: "center",
+                        data: this.opinion
+                    },
+                    series: [
+                        {
+                            name: "状态",
+                            type: "pie",
+                            radius: "65%",
+                            center: ["50%", "50%"],
+                            avoidLabelOverlap: false,
+                            itemStyle: {
+                                emphasis: {
+                                    shadowBlur: 10,
+                                    shadowOffsetX: 0,
+                                    shadowColor: "rgba(0, 0, 0, 0.5)"
+                                },
+                                color: function (params) {
+                                    //自定义颜色
+                                    var colorList = ["#1ab394", "#79d2c0"];
+                                    return colorList[params.dataIndex];
+                                }
+                            },
+                            data: this.opinionData
+                        }
+                    ]
+                });
+            },
+        },
+        created() {
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
